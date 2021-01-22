4f4021c500293e94feb1538c0d55b03530f57a82
Alp <alpcorreacastro@gmail.com>
2021-01-22 15:10:42 +0100

    add login validation

--- lib/util/login_page.dart
+++ lib/util/login_page.dart
@@ -9,10 +9,11 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageState extends State<LoginPage> {
-  String email = "peter@gmail.com";
-  String nickname = "P3ter";
-  String realname = "Peter";
-  String password = "abcd1234";
+  List<String> data = ['peter@gmail.com', 'P3ter', 'abcd1234'];
+  String name;
+  String password;
+
+  bool check;
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +40,7 @@ class LoginPageState extends State<LoginPage> {
                   ),
                   onChanged: (value) {
                     setState(() {
-                      email = value;
+                      name = value;
                     });
                   },
                 ),
@@ -47,6 +48,7 @@ class LoginPageState extends State<LoginPage> {
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
                 child: TextField(
+                  obscureText: true,
                   decoration: InputDecoration(
                     prefixIcon: Icon(Icons.lock),
                     hintText: 'Passwort',
@@ -58,15 +60,41 @@ class LoginPageState extends State<LoginPage> {
                   ),
                   onChanged: (value) {
                     setState(() {
-                      email = value;
+                      password = value;
                     });
                   },
                 ),
               ),
+              Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  child: MaterialButton(
+                    child: Text('Login'),
+                    color: Colors.blue,
+                    onPressed: () {
+                      if (_validation(name, password) == true ||
+                          _validation(name, password) == true) {
+                        print("Erfolgreicher Login");
+                      } else {
+                        print(
+                            "Nutzerdaten nicht vorhanden, bitte Probieren sie es erneut");
+                      }
+                    },
+                  )),
             ],
           ),
         ),
       ),
     );
+  }
+
+  bool _validation(String name, String password) {
+    if (data[0] == name || data[1] == name) {
+      if (password == data[2]) {
+        return true;
+      }
+    } else {
+      return false;
+    }
   }
 }
