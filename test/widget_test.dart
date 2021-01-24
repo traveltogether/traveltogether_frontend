bbec75e223ee1fef65d2f1ff3806f4d6f954b34f
Nika <j.kunow@outlook.de>
2021-01-24 14:11:21 +0100

    refactor folder structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:traveltogether_frontend/main.dart';
+import 'file:///C:/Users/admin/Documents/FH/SWE_II/traveltogether/traveltogether_frontend/lib/pages/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
