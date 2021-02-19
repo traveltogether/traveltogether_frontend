d055cf6acda0d9df0a3d25a1464d7fa62d45f822
Alp <alpcorreacastro@gmail.com>
2021-02-19 20:16:06 +0100

    merged register & login page into add_user_service

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:traveltogether_frontend/services/journey_service.dart';
+import 'package:traveltogether_frontend/services/user_service.dart';
 import '../widgets/request_and_offer_card.dart';
+import 'login_page.dart';
+import 'register_page.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -30,6 +34,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  UserService userService = new UserService();
   JourneyService journeyService = new JourneyService();
   String textfieldContent = "";
 
@@ -37,6 +42,19 @@ class _MyHomePageState extends State<MyHomePage> {
     this.journeyService.joinJourney(4).then((val) => setState(() {
           textfieldContent = val.toString();
         }));
+
+    this.userService.changeDisability(3,"testDis").then((value) => print("change disability: " + value.toString()));
+    userService.login("test", "test").then((value) => print("login: "+ value.toString()));
+
+    userService.changeUsername(3, "Test").then((value) => print(value));
+    userService.changeMail("test@test").then((value) => print("mail change: "+ value.toString()));
+    userService.changePassword("test", "test").then((value) => print("change password: "+ value.toString()));
+
+    this.userService.getUser(3).then((val) => setState(() {
+      print("meldung meldung!!!!!" + val.toString());
+      textfieldContent = val.disabilities.toString();
+    }));
+
   }
 
   @override
@@ -62,13 +80,27 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             ListTile(
               leading: Icon(Icons.mail),
-              title: Text("Nummer 1"),
-              onTap: () {},
+              title: Text("Register"),
+              onTap: () {
+                setState(
+                      () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => RegisterPage()));
+                  },
+                );
+              },
             ),
             ListTile(
               leading: Icon(Icons.mail),
-              title: Text("Nummer 2"),
-              onTap: () {},
+              title: Text("Login"),
+              onTap: () {
+                setState(
+                      () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => LoginPage()));
+                  },
+                );
+              },
             ),
             ListTile(
               leading: Icon(Icons.mail),
