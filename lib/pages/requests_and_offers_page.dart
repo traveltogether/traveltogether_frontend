28f79fd1d5fe47489060dc4d691744b2184a4e4d
Nika <j.kunow@outlook.de>
2021-03-05 19:22:04 +0100

    add dirty bugfix, add todo to improve it

--- lib/pages/requests_and_offers_page.dart
+++ lib/pages/requests_and_offers_page.dart
@@ -6,6 +6,7 @@ import 'package:traveltogether_frontend/view-models/user_read_view_model.dart';
 import 'package:traveltogether_frontend/widgets/request_and_offer_card.dart';
 
 class RequestsAndOffersPage extends StatefulWidget {
+  //ToDo: final UserReadViewModel user;
   final String pageType;
 
   const RequestsAndOffersPage(this.pageType, {Key key}) : super(key: key);
@@ -24,7 +25,10 @@ class _RequestsAndOffersPageState extends State<RequestsAndOffersPage> {
     super.initState();
     journeyService = new JourneyService();
     userService = new UserService();
-    userService.getCurrentUser().then((user) => currentUser = user);
+    userService.getCurrentUser().then((user) {
+      currentUser = user;
+      _refreshPage();
+    });
   }
 
   _refreshPage() {
@@ -39,13 +43,13 @@ class _RequestsAndOffersPageState extends State<RequestsAndOffersPage> {
       ),
       body: FutureBuilder<List<JourneyReadViewModel>>(
         future: journeyService.getAll(
-            openForRequests: true,
-            offer: widget.pageType == "offers" ? true : null,
-            request: widget.pageType == "requests" ? true : null,
+          openForRequests: true,
+          offer: widget.pageType == "offers" ? true : null,
+          request: widget.pageType == "requests" ? true : null,
         ),
         builder: (BuildContext context,
             AsyncSnapshot<List<JourneyReadViewModel>> snapshot) {
-          if (!snapshot.hasData || currentUser == null) {
+          if ((!snapshot.hasData || currentUser == null)) {
             return Center(child: CircularProgressIndicator());
           } else {
             List<JourneyReadViewModel> journeys = [];
