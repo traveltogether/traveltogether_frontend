55cea9a78c695766233da47cdc543a0895c2f09e
Alp <alpcorreacastro@gmail.com>
2021-03-08 10:10:40 +0100

    format Button

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -3,21 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 class Button extends StatelessWidget {
-
   String text;
   Color customTextColor;
   double customTextScale;
-  Button(this.text, {this.customTextColor = Colors.black87, this.customTextScale= 1.0});
+
+  Button(this.text,
+      {this.customTextColor = Colors.black87, this.customTextScale = 1.0});
+
   @override
   Widget build(BuildContext context) {
     return OutlineButton(
-        onPressed: null,
-        child: Text(
-          this.text,
-          style: TextStyle(color: this.customTextColor),
-          textScaleFactor: this.customTextScale,
-          textAlign: TextAlign.right,
-        ),
+      onPressed: null,
+      child: Text(
+        this.text,
+        style: TextStyle(color: this.customTextColor),
+        textScaleFactor: this.customTextScale,
+        textAlign: TextAlign.right,
+      ),
     );
   }
 }
