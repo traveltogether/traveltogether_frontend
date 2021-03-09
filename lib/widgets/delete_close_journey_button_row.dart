4094e58898619cc2b733ca047e11377529c02230
Nika <j.kunow@outlook.de>
2021-03-09 12:25:38 +0100

    add popup for reason when canceling a journey

--- lib/widgets/delete_close_journey_button_row.dart
+++ lib/widgets/delete_close_journey_button_row.dart
@@ -3,13 +3,22 @@ import 'package:traveltogether_frontend/services/journey_service.dart';
 import 'package:traveltogether_frontend/view-models/journey_read_view_model.dart';
 import 'package:traveltogether_frontend/widgets/pop_up.dart';
 
+import 'cancel_journey_pop_up.dart';
+
 class DeleteCloseJourneyButtonRow extends StatelessWidget {
   final JourneyReadViewModel journey;
   final void Function() refreshParent;
+  bool isDeletable;
   JourneyService journeyService;
 
   DeleteCloseJourneyButtonRow(this.journey, this.refreshParent) {
     this.journeyService = new JourneyService();
+    if (journey.pendingUserIds == null &&
+        journey.acceptedUserIds == null) {
+      isDeletable = true;
+    } else {
+      isDeletable = false;
+    }
   }
 
   @override
@@ -19,29 +28,57 @@ class DeleteCloseJourneyButtonRow extends StatelessWidget {
       children: [
         ElevatedButton(
             onPressed: (() {
-              this
-                  .journeyService
-                  .cancelJourney(journey.id, "reason")
-                  .then((response) {
-                if (response["error"] == null) {
-                  refreshParent();
-                } else {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return PopUp(
-                        "Fehler",
-                        response["error"] +
-                            "\n\nBitte kontaktiere den Support.",
-                        isWarning: true,
+              if (!isDeletable) {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return CancelJourneyPopUp();
+                  },
+                ).then((reason) {
+                  print(reason);
+                  this
+                      .journeyService
+                      .cancelJourney(journey.id, reason)
+                      .then((response) {
+                    if (response["error"] == null) {
+                      refreshParent();
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return PopUp(
+                            "Fehler",
+                            response["error"] +
+                                "\n\nBitte kontaktiere den Support.",
+                            isWarning: true,
+                          );
+                        },
                       );
-                    },
-                  );
-                }
-              });
+                    }
+                  });
+                });
+              } else {
+                this.journeyService.deleteJourney(journey.id).then((response) {
+                  if (response["error"] == null) {
+                    refreshParent();
+                  } else {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return PopUp(
+                          "Fehler",
+                          response["error"] +
+                              "\n\nBitte kontaktiere den Support.",
+                          isWarning: true,
+                        );
+                      },
+                    );
+                  }
+                });
+              }
             }),
-            child: Text("Löschen")),
-        OutlineButton(
+            child: Text(isDeletable ? "Löschen" : "Absagen")),
+        OutlinedButton(
             onPressed: (() {
               this
                   .journeyService
