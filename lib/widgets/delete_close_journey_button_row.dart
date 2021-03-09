0659f8864db4ddc0bb8e55382e5b24634bc373a0
Nika <j.kunow@outlook.de>
2021-03-09 14:35:40 +0100

    add deletion confirmation popup for journeys

--- lib/widgets/delete_close_journey_button_row.dart
+++ lib/widgets/delete_close_journey_button_row.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:traveltogether_frontend/services/journey_service.dart';
 import 'package:traveltogether_frontend/view-models/journey_read_view_model.dart';
+import 'package:traveltogether_frontend/widgets/journey_deletion_pop_up.dart';
 import 'package:traveltogether_frontend/widgets/pop_up.dart';
 
 import 'cancel_journey_pop_up.dart';
@@ -57,36 +58,48 @@ class DeleteCloseJourneyButtonRow extends StatelessWidget {
                   });
                 });
               } else {
-                this.journeyService.deleteJourney(journey.id).then((response) {
-                  if (response["error"] == null) {
-                    refreshParent();
-                  } else {
-                    if (response["error"] ==
-                        "deletion_not_available_due_to_requests") {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return PopUp(
-                            "Fehler",
-                            "Da es für diese Fahrt inzwischen Anfragen gibt, kannst du sie nicht mehr löschen. Wenn du sie immer noch entfernen möchtest kannst du sie absagen und einen Grund angeben.",
-                            isWarning: true,
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return JourneyDeletionPopUp();
+                  },
+                ).then((response) {
+                  if (response) {
+                    this
+                        .journeyService
+                        .deleteJourney(journey.id)
+                        .then((response) {
+                      if (response["error"] == null) {
+                        refreshParent();
+                      } else {
+                        if (response["error"] ==
+                            "deletion_not_available_due_to_requests") {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return PopUp(
+                                "Fehler",
+                                "Da es für diese Fahrt inzwischen Anfragen gibt, kannst du sie nicht mehr löschen. Wenn du sie immer noch entfernen möchtest kannst du sie absagen und einen Grund angeben.",
+                                isWarning: true,
+                              );
+                            },
                           );
-                        },
-                      );
-                      refreshParent();
-                    } else {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return PopUp(
-                            "Fehler",
-                            response["error"] +
-                                "\n\nBitte kontaktiere den Support.",
-                            isWarning: true,
+                          refreshParent();
+                        } else {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return PopUp(
+                                "Fehler",
+                                response["error"] +
+                                    "\n\nBitte kontaktiere den Support.",
+                                isWarning: true,
+                              );
+                            },
                           );
-                        },
-                      );
-                    }
+                        }
+                      }
+                    });
                   }
                 });
               }
