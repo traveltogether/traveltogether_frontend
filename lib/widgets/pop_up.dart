21dc41a8c09eb30c2c45de90e4cc25f73e42124e
Nika <j.kunow@outlook.de>
2021-03-09 16:50:14 +0100

    refactor pop up

--- lib/widgets/pop_up.dart
+++ lib/widgets/pop_up.dart
@@ -10,23 +10,35 @@ class PopUp extends StatelessWidget {
 
   Color titleColor = Colors.green;
 
-  PopUp(this.title, this.text, {this.answer = "OK", this.isWarning = false, this.isHighContrast = false});
+  PopUp(this.title, this.text,
+      {this.answer = "OK",
+      this.isWarning = false,
+      this.isHighContrast = false});
 
   @override
   Widget build(BuildContext context) {
-    if(isWarning==true) titleColor = Colors.red;
-    if(isHighContrast == true) titleColor = Colors.black;
+    if (isWarning == true) titleColor = Colors.red;
+    if (isHighContrast == true) titleColor = Colors.black;
 
-    Widget okButton = FlatButton(
-      child: Text(this.answer, style: TextStyle(color: Colors.blueAccent),),
+    Widget okButton = TextButton(
+      child: Text(
+        this.answer,
+        style: TextStyle(color: Colors.blueAccent),
+      ),
       onPressed: () {
         Navigator.of(context, rootNavigator: true).pop();
       },
     );
 
     return AlertDialog(
-      title: Text(this.title, style: TextStyle(color: this.titleColor),),
-      content: Text(this.text, style: TextStyle(color: Colors.black),),
+      title: Text(
+        this.title,
+        style: TextStyle(color: this.titleColor),
+      ),
+      content: Text(
+        this.text,
+        style: TextStyle(color: Colors.black),
+      ),
       backgroundColor: Colors.white,
       actions: [
         okButton,
